d24af084e66858c344cf67bd74ca9dc1e1a2a79d
Amit Kumar <iamamitkumar051@gmail.com>
2022-05-18 18:47:21 +0000

    Finish DRF views

--- blog/api_urls.py
+++ blog/api_urls.py
@@ -1,4 +1,5 @@
 from django.urls import path
+from rest_framework.urlpatterns import format_suffix_patterns
 
 from blog.api_views import post_list, post_detail
 
@@ -6,3 +7,5 @@ urlpatterns = [
     path("posts/", post_list, name="api_post_list"),
     path("posts/<int:pk>/", post_detail, name="api_post_detail"),
 ]
+
+urlpatterns = format_suffix_patterns(urlpatterns)
